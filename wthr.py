b7296bb79da561b164ca774886a64db1a06e0ff8
Giles <me@glavelle.co.uk>
2011-10-07 15:39:42 +0100

    Refactored, made it work with places outside California.

--- wthr.py
+++ wthr.py
@@ -5,12 +5,12 @@
 # Author:      Giles
 #
 # Created:     05/10/2011
-# Copyright:   (c) Giles 2011
-# Licence:     <your licence>
+# Copyright:   (c) Giles Lavelle 2011
+# Licence:     GPL v2
 #-------------------------------------------------------------------------------
 #!/usr/bin/env python
 
-import urllib2
+from urllib2 import urlopen
 import json
 import xml.etree.ElementTree as xml
 from optparse import OptionParser
@@ -26,43 +26,44 @@ parser.add_option("-p", "--place", dest="place", help="Get the weather in the gi
 parser.add_option("-u", "--unit", dest="unit", help="Specify the unit of the temperature", metavar="UNIT")
 
 options, args = parser.parse_args()
-#print options
-place = options.place
+place = options.place.replace("_", " ")
 unit = options.unit
 unit = unit if (unit == "c" or unit == "f") else "c"
 
 class Wthr:
     def _getIP(self):
-        return urllib2.urlopen("http://icanhazip.com").read()[:-1]
+        return urlopen("http://icanhazip.com").read()[:-1]
 
     def _getWOEID(self, place):
         try:
-            url = "http://query.yahooapis.com/v1/public/yql?q=select%20*%20from%20geo.places%20where%20text%3D%22" + place.replace(" ", "%20") + "%20CA%22&format=json"
-            response = json.loads(urllib2.urlopen(url).read())
+            url = "http://query.yahooapis.com/v1/public/yql?q=select%20*%20from%20geo.places%20where%20text%3D%22" + place.replace(" ", "%20") + "%22&format=json"
+            response = json.loads(urlopen(url).read())
             woeid = response["query"]["results"]["place"][0]["woeid"]
             return woeid
         except:
             return None
 
-    def getLocation(self):
+    def _getLocation(self):
             ip = self._getIP()
-            loc = urllib2.urlopen("http://api.hostip.info/get_html.php?ip=" + ip).read()
+            loc = urlopen("http://api.hostip.info/get_html.php?ip=" + ip).read()
             location = loc[loc.find("City: ") + 6 : loc.find("IP: ") - 1]
             if location == "(Unknown City?)":
-                print "Could not determine your location. Please enter a location instead."
+                print "Could not automatically find your location. Please enter a location instead."
                 return None
             return location
 
     def getWeather(self, place, unit):
         try:
-            woeid = self._getWOEID(place)
-            url = "http://weather.yahooapis.com/forecastrss?w=" + woeid + "&u=" + unit
-            response = xml.XML(urllib2.urlopen(url).read())
+            if place == None:
+                place = self._getLocation()
+            WOEID = self._getWOEID(place)
+            url = "http://weather.yahooapis.com/forecastrss?w=" + WOEID + "&u=" + unit
+            response = xml.XML(urlopen(url).read())
             title = response.find("channel/description").text[7:] + " on " + response.find("channel/item/pubDate").text + ":\n"
             raw = response.find("channel/item/description").text
-            start_string = "Forecast:</b><BR />"
-            end_string = "<a href"
-            weather = raw[raw.find(start_string) + len(start_string) : raw.find(end_string) - 1].replace("<br />", "")[:-1]
+            start = "Forecast:</b><BR />"
+            end = "<a href"
+            weather = raw[raw.find(start) + len(start) : raw.find(end) - 1].replace("<br />", "")[:-1]
             return title + weather
         except:
             return "Could not get weather"
@@ -70,8 +71,7 @@ class Wthr:
 wthr = Wthr()
 if place != None:
     if place == 'here':
-        location = wthr.getLocation()
-        print wthr.getWeather(location, unit)
+        print wthr.getWeather(None, unit)
     else:
         print wthr.getWeather(place, unit)
 else:
